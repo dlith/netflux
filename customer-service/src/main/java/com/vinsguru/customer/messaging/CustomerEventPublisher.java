6effd64f3be70461b3dda74fe78484a175bb82ef
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2026-03-21 12:28:57 +0100

    customer service: add service and event published impl

--- customer-service/src/main/java/com/vinsguru/customer/messaging/CustomerEventPublisher.java
+++ customer-service/src/main/java/com/vinsguru/customer/messaging/CustomerEventPublisher.java
@@ -1,15 +1,33 @@
 package com.vinsguru.customer.messaging;
 
 import com.vinsguru.netflux.events.CustomerGenreUpdatedEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.context.event.EventListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CustomerEventPublisher {
 
+    private static final Logger log = LoggerFactory.getLogger(CustomerEventPublisher.class);
+    private final StreamBridge streamBridge;
+    private static final String CUSTOMER_EVENTS_OUTS = "customer-events-out";
+
+    public CustomerEventPublisher(StreamBridge streamBridge) {
+        this.streamBridge = streamBridge;
+    }
+
     @EventListener
     public void onGenreUpdatedEvent(CustomerGenreUpdatedEvent genreUpdatedEvent) {
-
+        Message<CustomerGenreUpdatedEvent> message = MessageBuilder.withPayload(genreUpdatedEvent)
+                .setHeader(KafkaHeaders.KEY, genreUpdatedEvent.customerId())
+                .build();
+        this.streamBridge.send(CUSTOMER_EVENTS_OUTS, message);
+        log.info("published: {}", message);
     }
 
 }
--- customer-service/src/main/java/com/vinsguru/customer/service/CustomerService.java
+++ customer-service/src/main/java/com/vinsguru/customer/service/CustomerService.java
@@ -2,17 +2,36 @@ package com.vinsguru.customer.service;
 
 import com.vinsguru.customer.dto.CustomerDetails;
 import com.vinsguru.customer.dto.GenreUpdateRequest;
+import com.vinsguru.customer.entity.Customer;
+import com.vinsguru.customer.exception.CustomerNotFoundException;
+import com.vinsguru.customer.mapper.CustomerMapper;
+import com.vinsguru.customer.repository.CustomerRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerService {
 
-    public CustomerDetails getCustomer(Integer customerId) {
-        return null;
+    private final CustomerRepository customerRepository;
+    private final ApplicationEventPublisher applicationEventPublisher;
+
+    public CustomerService(CustomerRepository customerRepository, ApplicationEventPublisher applicationEventPublisher) {
+        this.customerRepository = customerRepository;
+        this.applicationEventPublisher = applicationEventPublisher;
     }
 
-    public void updateCustomerGenre(Integer customerId, GenreUpdateRequest request) {
+    public CustomerDetails getCustomer(Integer customerId) {
+        return this.customerRepository.findById(customerId).map(CustomerMapper::toCustomerDetails)
+                .orElseThrow(() -> new CustomerNotFoundException(customerId));
+    }
 
+    @Transactional
+    public void updateCustomerGenre(Integer customerId, GenreUpdateRequest request) {
+        Customer customer = this.customerRepository.findById(customerId)
+                .orElseThrow(() -> new CustomerNotFoundException(customerId));
+        customer.setFavoriteGenre(request.favoriteGenre());
+        this.applicationEventPublisher.publishEvent(CustomerMapper.toGenreUpdatedEvent(customerId, request.favoriteGenre()));
     }
 
 }
